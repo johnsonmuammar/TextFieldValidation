f72b967b71125b5d5b257e90d00d32c43495dbb5
Muammar Johnson <muammarj@gmail.com>
2022-02-02 13:24:52 -0500

    text box changes color depending on if its error or info red for error dark grey for info

--- app/src/main/java/com/example/textboxvalidationcomponent/textBoxValidationComponent/TextBoxValidationComponent.kt
+++ app/src/main/java/com/example/textboxvalidationcomponent/textBoxValidationComponent/TextBoxValidationComponent.kt
@@ -2,13 +2,21 @@ package com.example.textboxvalidationcomponent.textBoxValidationComponent
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import com.example.textboxvalidationcomponent.domain.model.Type
 import com.example.textboxvalidationcomponent.domain.model.ValidationObject
 import com.example.textboxvalidationcomponent.domain.use_case.validate_text.ValidateTextUseCase
@@ -19,38 +27,30 @@ val TAG = "Validated Text Field"
 fun ValidatedTextField(field_label:String ="",
                        errorValidationList:List<ValidationObject>
 
-){
+) {
     var validateTextUseCase = ValidateTextUseCase(errorValidationList)
-    var text by remember {
-        mutableStateOf("")
-    }
-    var infoText  by remember{
-        mutableStateOf("")}
-    var color by remember {
-     mutableStateOf(Color.Black)
-    }
+    var text by remember { mutableStateOf("") }
+    var infoText by remember { mutableStateOf("") }
+    var color by remember { mutableStateOf(Color.Black) }
+    var infoTextFlag by remember { mutableStateOf(false) }
 
-    var infoTextFlag by remember {
-        mutableStateOf(false)
-    }
-    fun validate(inputSting: String){
+    fun validate(inputSting: String) {
         text = inputSting
         val validationObject = validateTextUseCase.validateText(inputString = inputSting)
-         if (validationObject != null) {
+        if (validationObject != null) {
 
-             color =  if (validationObject.type == Type.ERROR) Color.Red else Color.DarkGray
-             infoText = validationObject.message
-             infoTextFlag = true
-            }else{
-                infoText = ""
-                infoTextFlag = false
-         }
-
+            color = if (validationObject.type == Type.ERROR) Color.Red else Color.DarkGray
+            infoText = validationObject.message
+            infoTextFlag = true
+        } else {
+            infoText = ""
+            infoTextFlag = false
+        }
 
 
     }
-    Column() {
-        OutlinedTextField(
+    Column(modifier = Modifier.padding(10.dp).semantics(mergeDescendants = true) {}) {
+        TextField(
             value = text,
             onValueChange = { newText ->
                 validate(newText)
@@ -62,13 +62,23 @@ fun ValidatedTextField(field_label:String ="",
             ),
             label = { Text(field_label) },
             modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.Transparent
+            ),
+            trailingIcon = {
+                if (infoTextFlag && color == Color.Red) {
+                    Icon(Icons.Filled.Info, "error", tint = Color.Red)
+                } else if (infoTextFlag && color == Color.DarkGray) {
+                    Icon(Icons.Filled.Info, "error", tint = Color.DarkGray)
+                }
+            },
+            isError = infoTextFlag && color == Color.Red
 
         )
-        if (infoTextFlag) {
-            Text(text = infoText, color = color )
-        }else{
-            Text(text = "")
-        }
+        Text(
+            text = if (infoTextFlag) infoText else "",
+            color = color
+        )
     }
 }
 
